7397dd7b456d2899f6925279b04b60d1cfe8d6ef
Juan Castillo <castillobgr@gmail.com>
2013-09-18 10:50:54 -0500

    Accelerometer and Gyroscope basic concept tests.

---
+++ Accelerometer Test/Assets/Scripts/Accelerometer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Accelerometer : MonoBehaviour {
+	
+	public float speed;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	 	Vector3 dir = Vector3.zero;
+        dir.z = Input.acceleration.y;
+        dir.x = -Input.acceleration.x;
+        if (dir.sqrMagnitude > 1)
+            dir.Normalize();
+        
+        dir *= Time.deltaTime;
+        transform.Translate(dir * speed * Time.deltaTime);
+	}
+}
---
+++ Gyro Test/Assets/Scripts/GyroTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GyroTest : MonoBehaviour {
+	
+	
+	private Quaternion cameraCorrection = Quaternion.identity;
+	private Quaternion initRotation = Quaternion.identity;
+	
+	// Use this for initialization
+	void Start () {
+		initRotation = this.transform.rotation;
+		Input.gyro.enabled = true;
+		CalculateCameraCorrection();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Slerp(transform.rotation, //transform.rotation, 0.2f);
+			new Quaternion(-Input.gyro.attitude.x, -Input.gyro.attitude.y, Input.gyro.attitude.z, Input.gyro.attitude.w) * cameraCorrection, 0.2f);
+	}
+
+	void CalculateCameraCorrection()
+	{
+		cameraCorrection = new Quaternion(-Mathf.Abs(initRotation.x - Input.gyro.attitude.x), 
+			-Mathf.Abs(initRotation.y - Input.gyro.attitude.y), -Mathf.Abs(initRotation.z - Input.gyro.attitude.z), 
+			-Mathf.Abs(initRotation.w - Input.gyro.attitude.w));
+	}
+}
