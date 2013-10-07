b5b08031fe60a9b26de53252854a8f576f412a84
Alejandro Gomez Lodoño <alegomez544@gmail.com>
2013-10-06 20:03:55 -0500

    Network Test added

---
+++ MultiCube/Assets/Scripts/BoxMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxMovement : MonoBehaviour 
+{
+	int speed = 5;
+
+	// Use this for initialization
+	void Start () 
+	{
+		 
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{	
+		if(networkView.isMine)
+		{
+			transform.Translate(new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * speed,
+											0,
+											Input.GetAxis("Vertical") * Time.deltaTime * speed));	
+		}
+		else
+		{
+			enabled = false;
+		}
+	}
+}
---
+++ MultiCube/Assets/Scripts/SphereScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereScript : MonoBehaviour {
+
+	void OnTriggerEnter()
+	{
+		networkView.RPC("hit",RPCMode.AllBuffered,new Vector3(1,0,0));
+		Debug.Log("hit");
+	}
+	
+	[RPC]
+	void hit (Vector3 color)
+	{
+		renderer.material.color = new Color(color.x, color.y, color.z, 1);
+	}
+}
